b562668b4bd4cd555aa3931ba5456b2fc85854f4
Jeremy Kendall <jeremy@jeremykendall.net>
2012-03-20 22:47:58 -0500

    Removes bad require

--- zf1/manifests/init.pp
+++ zf1/manifests/init.pp
@@ -11,6 +11,5 @@ class zf1 {
     exec { 'svn co http://framework.zend.com/svn/framework/standard/tags/release-1.11.11/library/Zend/ /usr/share/php/Zend/':
         path     => '/usr/bin',
         alias    => 'cozf1',
-        requires => [Package['svn'], Package['php5'],],
     }
 }

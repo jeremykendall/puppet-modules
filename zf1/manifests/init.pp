ae6c105dd43919aad6cfa535c76acd605f9dc921
Jeremy Kendall <jeremy@jeremykendall.net>
2012-03-20 22:45:08 -0500

    Corrects bad path, bad command in exec

--- zf1/manifests/init.pp
+++ zf1/manifests/init.pp
@@ -3,14 +3,12 @@
 class zf1 {
 
     exec { 'mkdir -p /usr/share/php/Zend/':
-        path   => 'bin',
+        path   => '/bin',
         alias  => 'mkzenddir',
         before => Exec['cozf1'],
     }
 
-    exec { 'svn co
-    http://framework.zend.com/svn/framework/standard/tags/release-1.11.11/library/Zend/
-    /usr/share/php/Zend/':
+    exec { 'svn co http://framework.zend.com/svn/framework/standard/tags/release-1.11.11/library/Zend/ /usr/share/php/Zend/':
         path     => '/usr/bin',
         alias    => 'cozf1',
         requires => [Package['svn'], Package['php5'],],

80096478b140734d5a3c60b75f134edfeffc8c00
Jeremy Kendall <jeremy@jeremykendall.net>
2012-03-24 14:35:24 -0500

    Updates zf1 to make phplib dir, checkout to phplib dir, then create symlink in include path

--- zf1/manifests/init.pp
+++ zf1/manifests/init.pp
@@ -2,14 +2,22 @@
 
 class zf1 {
 
-    exec { 'mkdir -p /usr/share/php/Zend/':
+    exec { 'mkdir -p /usr/share/phplib/':
         path   => '/bin',
         alias  => 'mkzenddir',
         before => Exec['cozf1'],
     }
 
-    exec { 'svn co http://framework.zend.com/svn/framework/standard/tags/release-1.11.11/library/Zend/ /usr/share/php/Zend/':
+    exec { 'svn co http://framework.zend.com/svn/framework/standard/tags/release-1.11.11/ /usr/share/phplib/ZendFramework-1.11.11/':
         path     => '/usr/bin',
         alias    => 'cozf1',
+        requires => Exec['mkzenddir'],
+        before   => Exec['zfln'],
+    }
+
+    exec { 'ln -s /usr/share/phplib/ZendFramework-1.11.11/library/Zend /usr/share/php/Zend':
+        path     => '/bin',
+        requires => Exec ['cozf1'],
+        alias    => 'zfln',
     }
 }

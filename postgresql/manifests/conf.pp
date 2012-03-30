fcf2a94deb064f280329b4928b37f67f0724345a
Jeremy Kendall <jeremy@jeremykendall.net>
2012-03-30 13:49:54 -0500

    Updates postgres to complete configuration of box

--- postgresql/manifests/conf.pp
+++ postgresql/manifests/conf.pp
@@ -1,9 +1,34 @@
 # postgresql::conf.pp
 
-class postgresql::conf {
-    augeas { 'pg_hba':
-        context => "/files/etc/postgresql/9.1/main/pg_hba.conf",
-        changes => "set *[type = 'local'][database = 'all'][user = 'all'][method = 'peer']/method md5",
+class postgresql::conf(
+    $method,
+    $template = 'postgresql/pg_hba.conf.erb',
+) {
+    #    augeas { 'pg_hba':
+    #    context => "/files/etc/postgresql/9.1/main/pg_hba.conf",
+    #    changes => "set *[type = 'local'][database = 'all'][user = 'all'][method = 'peer']/method md5",
+    #    require => Package['postgresql'],
+    # }
+
+    file { "/etc/postgresql/9.1/main/pg_hba.conf":
+        content => template($template),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
         require => Package['postgresql'],
+    }
+
+    file { '/tmp/user-config.sql':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['postgresql'],
+        before  => Exec['user-config'],
+    }
+
+    exec { 'psql < /tmp/user-config.sql':
+        path    => '/usr/bin',
+        require => File['/tmp/user-config.sql'],
     }
 }

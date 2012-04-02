f5c5a78f9339290354d0c19a04b751285f54f36a
Jeremy Kendall <jeremy@jeremykendall.net>
2012-04-02 00:06:28 -0500

    Removes server specific config

--- apache2/manifests/init.pp
+++ apache2/manifests/init.pp
@@ -12,19 +12,4 @@ class apache2 {
       hasrestart => true,
       require    => Package['apache2'],
     }
-
-    file { "/etc/apache2/mods-enabled/rewrite.load":
-        ensure => symlink,
-        target => "/etc/apache2/mods-available/rewrite.load",
-        notify => Service['apache2'],
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
-    }
-
-    file { "/etc/apache2/sites-enabled/000-default":
-        ensure => absent,
-        notify => Service['apache2'],
-    }
-
-  }
+}

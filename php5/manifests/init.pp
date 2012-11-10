5c344153c0a376f5cfe63ad9d4cac73b362887b1
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-10 06:37:13 -0600

    Adds php5-cli

--- php5/manifests/init.pp
+++ php5/manifests/init.pp
@@ -5,6 +5,10 @@ class php5 {
         ensure => latest,
     }
 
+    package { 'php5-cli':
+        ensure => latest,
+    }
+
     $phpTimezone = '/bin/sed -i "s/^;date.timezone =/date.timezone = \'America\/Chicago\'/g" /etc/php5/apache2/php.ini'
     $phpShortOpenTag = '/bin/sed -i "s/^short_open_tag = On/short_open_tag = Off/g" /etc/php5/apache2/php.ini'
 

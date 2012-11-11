e00999d8808b9ce15a811481566f5fd8fe661170
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-11 16:34:29 -0600

    Turns HTML errors on

--- php5/manifests/init.pp
+++ php5/manifests/init.pp
@@ -7,8 +7,9 @@ class php5 {
 
     $phpTimezone = '/bin/sed -i "s/^;date.timezone =/date.timezone = \'America\/Chicago\'/g" /etc/php5/apache2/php.ini'
     $phpShortOpenTag = '/bin/sed -i "s/^short_open_tag = On/short_open_tag = Off/g" /etc/php5/apache2/php.ini'
+    $phpHtmlErrors = '/bin/sed -i "s/^html_errors = Off/html_errors = On/g" /etc/php5/apache2/php.ini'
 
-    exec { [ $phpTimezone, $phpShortOpenTag ]:
+    exec { [ $phpTimezone, $phpShortOpenTag, $phpHtmlErrors ]:
         require => Package['php5'],
         notify  => Service['apache2'],
     }

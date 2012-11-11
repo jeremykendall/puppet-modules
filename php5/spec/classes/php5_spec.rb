c274dbcfae948023d442f1787c795f069f5a7989
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-11 17:35:09 -0600

    Adds exec to turn html_errors on, refactors manifest and test

--- php5/spec/classes/php5_spec.rb
+++ php5/spec/classes/php5_spec.rb
@@ -10,7 +10,20 @@ describe 'php5', :type => :class do
         should contain_package('php5').with(
             'ensure' => 'latest'
         )
-        should contain_exec('/bin/sed -i "s/^;date.timezone =/date.timezone = \'America\/Chicago\'/g" /etc/php5/apache2/php.ini')
-        should contain_exec('/bin/sed -i "s/^short_open_tag = On/short_open_tag = Off/g" /etc/php5/apache2/php.ini')
+        
+        should contain_exec('date_timezone').with(
+            'command' => '/bin/sed -i "s/^;date.timezone =/date.timezone = \'America\/Chicago\'/g" /etc/php5/apache2/php.ini',
+            'onlyif'  => 'grep -c ";date.timezone =" /etc/php5/apache2/php.ini'
+        )
+
+        should contain_exec('short_tags_off').with(
+            'command' => '/bin/sed -i "s/^short_open_tag = On/short_open_tag = Off/g" /etc/php5/apache2/php.ini',
+            'onlyif'  => 'grep -c "short_open_tag = On" /etc/php5/apache2/php.ini'
+        )
+
+        should contain_exec('html_errors_on').with(
+            'command' => '/bin/sed -i "s/^html_errors = Off/html_errors = On/g" /etc/php5/apache2/php.ini',
+            'onlyif'  => 'grep -c "html_errors = Off" /etc/php5/apache2/php.ini'
+        )
     end
 end
